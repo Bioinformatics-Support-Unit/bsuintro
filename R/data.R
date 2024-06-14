969d6b24b9738908e40b791fb0aa4268653d314d
John Casement <john.casement@newcastle.ac.uk>
2024-06-14 17:59:44 +0100

    Changes to RData file

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' Table of differential expression analysis results (microarray data)
 #'
-#' A dataset containing the normalised counts for 36014
-#' genes in 22 samples.
+#' A dataset containing the normalised counts for 22239
+#' probes
 #'
 #' @docType data
-#' @format A tibble with 36014 rows and 23 columns
+#' @format A tibble with 22239 rows and 6 columns
 #'
 "Ctrl_vs_TF1"
 
