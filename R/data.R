70e298866045bacf45a572710e6dbbcb91a7c70b
John Casement <john.casement@newcastle.ac.uk>
2024-06-14 17:43:43 +0100

    Added R package for intro course

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Table of differential expression analysis results (microarray data)
+#'
+#' A dataset containing the normalised counts for 36014
+#' genes in 22 samples.
+#'
+#' @docType data
+#' @format A tibble with 36014 rows and 23 columns
+#'
+"Ctrl_vs_TF1"
+
